abd9265f3f0592b85baa34f268885ce811a34df5
syam47 <syam.ps1804@gmail.com>
2024-02-16 23:26:51 +0100

    Info update

--- main_window.py
+++ main_window.py
@@ -10,7 +10,7 @@ import tkinter.messagebox
 class MainWindow:
     def __init__(self, window):
         self.window = window
-        self.window.geometry("600x600")
+        self.window.geometry("800x500")
         self.data = EditFile("bucketlist.dat").read_from_file()
         self.setup_ui()
         self.selected_country = None
@@ -21,51 +21,57 @@ class MainWindow:
 
         self.title_app = tk.Label(self.window, text="My Bucketlist Places",fg="white",
                                bg="black", relief="solid", font=("arial",16,"bold"))
-        self.title_app.pack(fill=tk.BOTH, pady=20, padx=0, expand=False)
+        self.title_app.place(x=0, y=10, width=800)
 
         self.country_label = tk.Label(self.window, text="Country")
-        self.country_label.place(x=20, y=60)
+        self.country_label.place(x=20, y=50)
 
-        self.country_entry = tk.Entry(self.window)
-        self.country_entry.pack(fill=tk.BOTH, pady=15, padx=20, expand=False)
+        self.country_entry = tk.Entry(self.window, width=50)
+        self.country_entry.place(x=20, y=70, width=580)
 
         self.place_label = tk.Label(self.window, text="Place")
-        self.place_label.place(x=20, y=110)
+        self.place_label.place(x=20, y=90)
 
         self.place_entry = tk.Entry(self.window)
-        self.place_entry.pack(fill=tk.BOTH, pady=15, padx=20, expand=False, )
+        self.place_entry.place(x=20, y=110, width=580)
 
-        self.add_button_frame = tk.Frame(self.window)
-        self.add_button_frame.pack(side=tk.LEFT, fill=tk.Y, padx=20)
+        self.add_button = tk.Button(self.window, text='ADD', command=self.add_item)
+        self.add_button.place(x=640, y=60, width=50)
 
-        self.add_button = tk.Button(self.add_button_frame, text='ADD', command=self.add_item)
-        self.add_button.pack(anchor=tk.E)
+        self.delete_button = tk.Button(self.window, text="DELETE", command=self.delete_place)
+        self.delete_button.place(x=720, y=60, width=50)
 
-        self.delete_button_frame = tk.Frame(self.window)
-        self.delete_button_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=20)
+        self.overwrite_button = tk.Button(self.window, text="OVERWRITE", command=self.overwrite_info)
+        self.overwrite_button.place(x=640, y=105, width=130)
 
-        self.delete_button = tk.Button(self.delete_button_frame, text="DELETE", command=self.delete_place)
-        self.delete_button.pack(anchor=tk.E)
+        self.listboxes()
 
+
+    def listboxes(self):
         self.country_listbox = tk.Listbox(self.window)
-        self.country_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.country_listbox.place(x=20, y=140, width=200, height=340)
         self.country_scroller = tk.Scrollbar(self.country_listbox, command=self.country_listbox.yview)
         self.country_scroller.pack(side=tk.RIGHT, fill=tk.Y)
         self.country_listbox.config(yscrollcommand=self.country_scroller.set)
 
         for item in sorted(self.data.keys()):
-            self.country_listbox.insert(tk.END, item)
+            if item !="INFO":
+                self.country_listbox.insert(tk.END, item)
 
         self.place_listbox = tk.Listbox(self.window)
-        self.place_listbox.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        self.place_listbox.place(x=240, y=140, width=200, height=340)
         self.place_scroller = tk.Scrollbar(self.place_listbox, command=self.place_listbox.yview)
         self.place_scroller.pack(side=tk.RIGHT, fill=tk.Y)
         self.place_listbox.config(yscrollcommand=self.place_scroller.set)
 
+        self.info_box = tk.Text(self.window, wrap="word")
+        self.info_box.place(x=460, y=140, width=310, height= 340)
+        self.info_box_scroll = tk.Scrollbar(self.info_box, command=self.place_listbox.yview)
+        self.info_box_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        self.info_box.config(yscrollcommand=self.info_box_scroll.set)
+
         # Bind the selection event to the on_select function
         self.country_listbox.bind("<<ListboxSelect>>", self.place_list)
-
-
 
           
     def add_item(self):
@@ -76,6 +82,7 @@ class MainWindow:
         if country != "":
             if country.lower() not in [x.lower() for x in self.data.keys()]:
                 self.data[country] = []
+                self.data["INFO"][country] = ""
 
             if place=="":
                 tkinter.messagebox.showinfo("Warning!", 'Place is not given.')
@@ -86,9 +93,6 @@ class MainWindow:
 
                 self.country_entry.delete(0, tk.END)
                 self.place_entry.delete(0, tk.END)
-
-            
-
 
         else:
             tkinter.messagebox.showinfo("Warning!", 'Give the country name please!')
@@ -103,7 +107,8 @@ class MainWindow:
 
         self.country_listbox.delete(0, tk.END)
         for item in sorted(self.data.keys()):
-            self.country_listbox.insert(tk.END, item)
+            if item!="INFO":
+                self.country_listbox.insert(tk.END, item)
 
 
 
@@ -118,8 +123,15 @@ class MainWindow:
         for item in sorted(self.data[self.selected_country]):
             self.place_listbox.insert(tk.END, item)
 
+        self.info_box.delete("1.0", tk.END)
+
+
         self.place_listbox.bind("<<ListboxSelect>>", self.on_select)
 
+
+        self.info_box.insert(tk.END, self.data["INFO"][self.selected_country] + "\n")
+
+    
 
 
     def on_select(self, event):
@@ -139,6 +151,12 @@ class MainWindow:
 
 
         self.update_country_and_place_lists()
+
+
+    def overwrite_info(self):
+        info = self.info_box.get("1.0", tk.END)
+        self.data["INFO"][self.selected_country] = info
+        EditFile("bucketlist.dat", self.data).overwrite_file()
 
 
 
---
+++ test2.py
@@ -0,0 +1,8 @@
+from dat import EditFile
+
+
+
+data2 = EditFile("bucketlist.dat")
+data = data2.read_from_file()
+print(data)
+
